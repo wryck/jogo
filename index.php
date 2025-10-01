4aa76d82839e1f660059b53f79a9eb256ab6f5ea
wryck <60793678+wryck@users.noreply.github.com>
2025-10-01 19:27:37 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-// Exemplo de um script que redireciona para outra página na mesma pasta
-if (!headers_sent()) { // Verificando se os cabeçalhos já foram enviados
-    header("Location: index.html");
-    exit;
-}
+header('Location: index.html');
+exit; 
 ?>
